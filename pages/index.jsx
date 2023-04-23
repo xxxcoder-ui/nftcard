f98e34ecadabd7603b5aa6dffaefb2701eb22f76
xxxcoder-ui <yogamusik15@gmail.com>
2023-04-23 05:26:36 -0700

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/NftCreator.module.css";
+import NFTCreatorPage from "../components/nftCreator";
+
+export default function Home() {
+  return (
+    <>
+      <NFTCreatorPage/>
+    </>
+  );
+}
